7f934bb06ce348c5fc9f277663a53e59e6fb8e11
Sayyid Y <sayyidyofa@gmail.com>
2021-04-13 11:46:33 +0700

    add default time

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,10 @@ import { PrismaClient, Salary } from '@prisma/client';
 @Injectable()
 export class AppService {
   async getHello(): Promise<Salary> {
-    return new PrismaClient().salary.findMany().then(results => results[0]);
+    return new PrismaClient()
+    .salary
+    .findMany()
+    .then(results => results[0]);
   }
 
   getMe(): string {
